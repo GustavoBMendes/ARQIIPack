18ec48a28f15c96fdd419b5c990e401563e53c6e
GustavoBMendes <mendes.gustavob@gmail.com>
2019-04-23 21:59:11 -0300

    Novos códigos R dos gráficos

--- fontes/missRate.R
+++ fontes/missRate.R
@@ -1,7 +1,7 @@
-acertos <- matrix(c(34.659,19.726,135.611,2.218, 7.473,
-                    1.350,181.931,4.095,108.375,37.353,
-                    34.052,20.501,160.757,2.902, 7.748,
-                    1.547,94.29,14.293,260.359, 43.758), ncol=10, byrow=TRUE)
+acertos <- matrix(c(3.29375,20.08875,2.4175,1.0575,11.605,
+                    5.2125,5.54,5.9625,6.22875,2.83125,
+                    3.24125,20.92375,2.87,1.38375, 12.1975,
+                    6.29125,6.23375,9.3375,7.05652,3.30625), ncol=10, byrow=TRUE)
 
 colnames(acertos) <- c("BT","CG","DC","EP","FT","IS","LU","MG","SP","UA")
 rownames(acertos) <- c("Arquitetura 1","Arquitetura 2")
@@ -10,10 +10,10 @@ ac_data <- as.table(acertos)
 ac_data 
 
 par(xpd = TRUE, mar=c(5, 5, 5, 5), oma=c(0,0,0,0))
-barplot(as.matrix(ac_data), main = "Número médio de cache miss",
-        ylab="Em Milhões de dados", cex.lab=0.8,
+barplot(as.matrix(ac_data), main = "Cache rate",
+        ylab="Em (%)", cex.lab=0.8,
         cex.axis = 0.8,
-        beside=TRUE, ylim=c(0, 200), xpd = FALSE,
+        beside=TRUE, ylim=c(0, 25), xpd = FALSE,
         cex.names = 0.8,
         col=grey.colors(n = 2, start = 0.1, end = 0.8))
 
